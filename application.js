e556b27f8d6a5cc21fde070914593089bb72cd50
Kate Ciurej <kate.ciurej@gmail.com>
2014-02-22 15:30:54 -0500

    Adding venue names from Foursquare lists

--- application.js
+++ application.js
@@ -1,19 +1,44 @@
-//$(document).ready(function() {
+$(document).ready(function() {
 
-	// To do: Set var for token, but need to parse token from URL:
-	// "If your app is pure Javascript, you can easily parse the token from the URL."
-	// 
-	// What do I pass in for the &callback= parameter?
+	var accessToken = getAnchor();
+	var listIds = new Array();
+	var 
 
-	$.getJSON("https://api.foursquare.com/v2/users/self/lists?oauth_token=1HL25ZFN00KCGIVC4GFAKRBEKUITWEOVH1FXKRY2MCCKI5XT&v=20140114&callback=?",
-		
-	// Trying to mimic the code on JSONP section of http://jqfundamentals.com/chapter/ajax-deferreds
-	// Having some trouble!
-	// What does resp.results.length do, show length of JSON object?
+	if (accessToken !== null) {
+		var url = "https://api.foursquare.com/v2/users/self/lists?oauth_token=" + accessToken + "&v=20140210";
 	
-		function(resp) {
-			$('li').html('Results: ' + resp.results.length);
-	});
-});
+		$.getJSON(url, function(data) {
+
+			for (var i = data.response.lists.groups[0].items.length - 1; i >= 0; i--) {
+				var listName = data.response.lists.groups[0].items[i].name;
+				var listId = data.response.lists.groups[0].items[i].id;
+				listIds.push(listId);
+				$('ul').append('<li>' + listName + '</li>');
+			}
+		})
+		
+		.then(function(){
+			$.getJSON("https://api.foursquare.com/v2/lists/" + listIds[0] + "?oauth_token=" + accessToken + "&v=20140210", function(data) {
+				for (var key in data.response.list.listItems.items) {
+					var venueName = data.response.list.listItems.items[key].venue.name;
+					var venuePhone = data.response.list.listItems.items[key].venue.contact.phone;
+					var venueCity = data.response.list.listItems.items[key].venue.location.city;
+					var venueAddress = data.response.list.listItems.items[key].venue.location.address;
+					$('body').append('<p>' + venueName + '</p>');
+						}
+					});
+		});
+
+	function getAnchor() {
+		var fullHash = location.hash;
+		if (fullHash !== "") {
+			var x = fullHash.split('=');
+			return x[1];
+		} else {
+			return null;
+		}
+	}
+	
+	}
 
 });
